d97fed3ef077e47e19c61785f884ea3150e65170
trippl <tlaursen8@yahoo.com>
2024-04-05 18:49:02 -0400

    Array map method lab

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
+
+/*function titleCased() {
+  return tutorials.map(function(tutorial) {
+    let words = tutorial.split(' ')
+    
+    let titleCaseWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+
+    return titleCaseWords.join(' ')
+  })
+}*/
+
+// Simplify this code block:
 
 const titleCased = () => {
   return tutorials.map (title => {
@@ -18,3 +32,5 @@ const titleCased = () => {
       }).join(' ');
     });
   } 
+
+  console.log(titleCased())
